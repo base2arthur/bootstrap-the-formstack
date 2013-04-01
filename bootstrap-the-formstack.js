f2acd8965834f73cf95e1bc44047843758a076ec
Robbie Mitchell <robbie@knewton.com>
2013-03-31 21:15:07 -0400

    Add some comments

--- bootstrap-the-formstack.js
+++ bootstrap-the-formstack.js
@@ -1,37 +1,31 @@
 ;(function($) {
 	$(function() {
+		// Remove the Formstack styles
 		$('.fsBody link').remove();
 		$('.fsBody style').remove();
 
+		// Set the page up to be responsive
 		$('.fsPage')
 			.removeClass('fsPage')
 			.addClass('row-fluid');
 
+		// Remove Formstack junk
 		$('.fsSection')
 			.removeClass('fsSection');
 
+		// Set up a 1-col div to hold response spans
 		$('.fs1Col')
 			.removeClass('fs1Col')
 			.addClass('row-fluid');
 
-		// $('.fs2Col')
-		// 	.removeClass('fs2Col')
-		// 	.addClass('span6');
-
-		// $('.fs3Col')
-		// 	.removeClass('fs3Col')
-		// 	.addClass('span4');
-
-		// $('.fs4Col')
-		// 	.removeClass('fs4Col')
-		// 	.addClass('span3');
-
+		// Replace crufty row divs with a Bootstrap row container
 		$('.fsRow.fsFieldRow.fsLastRow')
 			.removeClass('fsRow')
 			.removeClass('fsFieldRow')
 			.removeClass('fsLastRow')
 			.addClass('row-fluid');
-			
+
+		// Map checkbox form elements to what Bootstrap is expecting			
 		$('.fsOptionLabel > input[type="checkbox"]').each(function(el, i) {
 			$(this)
 				.parent()
@@ -39,6 +33,11 @@
 				.addClass('checkbox');
 		});
 
+		$('.checkbox.horizontal')
+			.removeClass('horizontal')
+			.addClass('inline');
+
+		// Map radio form elements to what Bootstrap is expecting
 		$('.fsOptionLabel > input[type="radio"]').each(function(el, i) {
 			$(this)
 				.parent()
@@ -50,26 +49,29 @@
 			.removeClass('horizontal')
 			.addClass('inline');
 
-		$('.checkbox.horizontal')
-			.removeClass('horizontal')
-			.addClass('inline');
-
+		// Map supporting text to what Bootstrap is expecting
 		$('.fsSupporting')
 			.removeClass('fsSupporting')
 			.addClass('help-block');
 			
+		// Bootstrap the submit button	
 		$('.fsSubmitButton')
 			.removeClass('fsSubmitButton')
 			.addClass('btn');
 
-
+		// Remove manual styles from all inputs
 		$('input', $('.fsBody')).attr('style', '');
 
+		// Map multi-field groups to what Bootstrap is expecting
 		$('.fieldset-content')
 			.removeClass('fieldset-content')
 			.addClass('controls').addClass('controls-row');
 
-
+		// Fix multi-field city/state/zip address fields so they renderin inline properly
+		// 1. Find a Formstack div and rename it to a Bootstrapped div for multiple fields
+		// 2. Create a clone div beneath the first that we just renamed
+		// 3. In the first div, give each input and matching label the appropriate span
+		// 4. Move the labels into the second Bootstrapped div so the spans line up properly
 		function restructure_fields(container_class, span_class, context, new_div) {
 			var container = $('div.' + container_class, context);
 			$('input, label, select', container).addClass(span_class);
